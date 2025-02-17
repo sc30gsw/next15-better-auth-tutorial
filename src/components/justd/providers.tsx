175ef8acae0a133d8ae13349bf2eae950dcfd3e4
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-17 12:06:35 +0900

    💄 必要なコンポーネントのinstall

---
+++ src/components/justd/providers.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { RouterProvider } from 'react-aria-components'
+import { ThemeProvider } from '~/components/justd/theme-provider'
+
+declare module 'react-aria-components' {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>['push']>[1]
+    >
+  }
+}
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
+  return (
+    <RouterProvider navigate={router.push}>
+      <ThemeProvider enableSystem attribute="class">
+        {children}
+      </ThemeProvider>
+    </RouterProvider>
+  )
+}
