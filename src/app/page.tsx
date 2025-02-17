dc965c21139938df029c754c3e84067f341ceeb6
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-17 14:26:23 +0900

    🛂 sessionがない場合sign-inページにリダイレクト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
+import { getServerSession } from '~/lib/auth/get-server-session'
 
-export default function Home() {
-  return null
+export default async function Home() {
+  const session = await getServerSession()
+  return <div>{session?.user.id}</div>
 }
