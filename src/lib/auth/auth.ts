198969a56356f336eab5f410e77eed6c71ef3312
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-17 14:22:26 +0900

    🚚 フォルダ移動

---
+++ src/lib/auth/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { nextCookies } from 'better-auth/next-js'
+import { twoFactor } from 'better-auth/plugins'
+import { passkey } from 'better-auth/plugins/passkey'
+import { env } from '~/env'
+import { db } from '~/lib/db/db'
+import * as schema from '~/lib/db/schema'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'sqlite',
+    schema: schema,
+    usePlural: true,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
+  plugins: [twoFactor(), passkey(), nextCookies()],
+})
