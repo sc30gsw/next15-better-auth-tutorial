a6e1381e9acefdb0749373268fc6917cea21f188
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-17 14:24:54 +0900

    💄 Toast追加

--- src/components/justd/providers.tsx
+++ src/components/justd/providers.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation'
 import { RouterProvider } from 'react-aria-components'
 import { ThemeProvider } from '~/components/justd/theme-provider'
+import { Toast } from '~/components/justd/ui'
 
 declare module 'react-aria-components' {
   interface RouterConfig {
@@ -18,6 +19,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <RouterProvider navigate={router.push}>
       <ThemeProvider enableSystem attribute="class">
+        <Toast />
         {children}
       </ThemeProvider>
     </RouterProvider>
