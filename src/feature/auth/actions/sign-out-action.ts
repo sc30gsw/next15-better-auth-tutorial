f2fa6d1c49feec7f39a8b44dac92dee65786e186
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-18 07:36:05 +0900

    ✨ sign-out時キャッシュを削除する

--- src/feature/auth/actions/sign-out-action.ts
+++ src/feature/auth/actions/sign-out-action.ts
@@ -1,15 +1,15 @@
 'use server'
 
-import { headers } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { auth } from '~/lib/auth/auth'
+import { revalidateTag } from 'next/cache'
+
+import {
+  GET_PASS_KEY,
+  GET_TWO_FACTOR,
+  GET_USER_DETAILS,
+} from '~/feature/auth/constants/cache-kyes'
 
 export const signOutAction = async () => {
-  const result = await auth.api.signOut({ headers: await headers() })
-
-  if (!result.success) {
-    redirect('/')
-  }
-
-  redirect('/sign-in')
+  revalidateTag(GET_TWO_FACTOR)
+  revalidateTag(GET_PASS_KEY)
+  revalidateTag(GET_USER_DETAILS)
 }
