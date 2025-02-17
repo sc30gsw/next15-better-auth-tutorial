ca87d5f61418261ab4d943e956494fa3397ca383
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-02-17 11:37:30 +0900

    🚑 認証のSchema作成

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,8 +1,55 @@
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const testTable = sqliteTable('tests', {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text().notNull(),
-  age: int().notNull(),
-  email: text().notNull().unique(),
+export const users = sqliteTable('users', {
+  id: text('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  emailVerified: int('email_verified', { mode: 'boolean' }).notNull(),
+  image: text('image'),
+  createdAt: int('created_at', { mode: 'timestamp' }).notNull(),
+  updatedAt: int('updated_at', { mode: 'timestamp' }).notNull(),
+})
+
+export const sessions = sqliteTable('sessions', {
+  id: text('id').primaryKey(),
+  expiresAt: int('expires_at', { mode: 'timestamp' }).notNull(),
+  token: text('token').notNull().unique(),
+  createdAt: int('created_at', { mode: 'timestamp' }).notNull(),
+  updatedAt: int('updated_at', { mode: 'timestamp' }).notNull(),
+  ipAddress: text('ip_address'),
+  userAgent: text('user_agent'),
+  userId: text('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+})
+
+export const accounts = sqliteTable('accounts', {
+  id: text('id').primaryKey(),
+  accountId: text('account_id').notNull(),
+  providerId: text('provider_id').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  accessToken: text('access_token'),
+  refreshToken: text('refresh_token'),
+  idToken: text('id_token'),
+  accessTokenExpiresAt: int('access_token_expires_at', {
+    mode: 'timestamp',
+  }),
+  refreshTokenExpiresAt: int('refresh_token_expires_at', {
+    mode: 'timestamp',
+  }),
+  scope: text('scope'),
+  password: text('password'),
+  createdAt: int('created_at', { mode: 'timestamp' }).notNull(),
+  updatedAt: int('updated_at', { mode: 'timestamp' }).notNull(),
+})
+
+export const verifications = sqliteTable('verifications', {
+  id: text('id').primaryKey(),
+  identifier: text('identifier').notNull(),
+  value: text('value').notNull(),
+  expiresAt: int('expires_at', { mode: 'timestamp' }).notNull(),
+  createdAt: int('created_at', { mode: 'timestamp' }),
+  updatedAt: int('updated_at', { mode: 'timestamp' }),
 })
